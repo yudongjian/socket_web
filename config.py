e8f937e49ce1f902c2c1f02dd6d59d44b594c10e
余东建 <2516234365@qq.com>
2021-01-28 09:52:16 +0000

    Update config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+
+# 数据库配置信息
+
+sql_config = {
+    'user': 'root',
+    'password': '123456',
+    'host': '180.3.15.63',
+    'database': "shanghai",
+}
+#  html网页路径
+base_path = './html/'
+
+login_path =  base_path +'login.html'
+register_path =  base_path +'register.html'
+set_path =  base_path +'set.html'
+menu_path =  base_path +'menu.html'
+
+# 下载文件路径
+down_path=  './down/download.txt'
+
+
+# IP 端口
+host = '180.3.15.63'
+port = 8080
+
+
+
